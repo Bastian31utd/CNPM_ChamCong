90fa0a02569690154c96810c2cd659b145c3f857
Vũ đẹp trai <anhvu200376@gmail.com>
2023-12-29 14:15:45 +0700

    vu bui dep trai

--- src/main/java/com/example/cnpm/AfterLoginUser.java
+++ src/main/java/com/example/cnpm/AfterLoginUser.java
@@ -1,22 +1,34 @@
 package com.example.cnpm;
 
+import com.example.cnpm.DatabaseClass.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AfterLoginUser extends Application {
     private static Stage currentStage;
     @Override
     public void start(Stage stage) throws IOException {
         currentStage = stage;
         stage.setResizable(false);
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UserProfile.fxml"));
+        DataBaseConnector db = new DataBaseConnector();
+        db.connect();
+        User user = db.getUserProfileFromId("C31");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AdminProfile.fxml"));
+        Parent root = fxmlLoader.load();
+        // Create a new stage
+        AdminProfile controller = fxmlLoader.getController();
+        db.disconnect();
+        System.out.println(user.getName());
+        controller.setUser(user);
         stage.setTitle("Hệ thống quản lý chấm công");
-        stage.setScene(new Scene(fxmlLoader.load(), 600, 400));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(new Scene(root, 600, 400));
         stage.show();
     }
     public void changeScene(String fxml) throws IOException {
