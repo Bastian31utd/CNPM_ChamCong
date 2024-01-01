5ae6d3989554edf6017c45d66751fb76f633eaf1
Trinhh <trinhhoanganh.pl@gmail.com>
2024-01-01 09:33:37 +0700

    backup

--- src/main/java/com/example/cnpm/HelloApplication.java
+++ src/main/java/com/example/cnpm/HelloApplication.java
@@ -5,27 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Node;
-import javafx.event.ActionEvent;
-
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     private static Stage stg;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        stg=primaryStage;
+    public void start(Stage primaryStage) throws Exception {
+        stg = primaryStage;
         primaryStage.setResizable(false);
         Parent root = FXMLLoader.load(getClass().getResource("start-view.fxml"));
-        //primaryStage.setTitle("Log in");
+        primaryStage.setTitle("Hệ thống quản lý chấm công");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
+
     public void changeScene(String fxml) throws IOException {
         Parent pane = FXMLLoader.load(getClass().getResource(fxml));
         stg.getScene().setRoot(pane);
     }
+
     public void changeSceneToHomeAdmin(String fxml, String userID) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource(fxml));
@@ -39,6 +43,7 @@ public class HelloApplication extends Application {
         stg.setScene(scene);
         stg.show();
     }
+
     public void changeSceneToHomeuser(String fxml, String userID) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource(fxml));
@@ -52,6 +57,4 @@ public class HelloApplication extends Application {
         stg.setScene(scene);
         stg.show();
     }
-
-    public static void main(String[] args) { launch(args); }
 }
