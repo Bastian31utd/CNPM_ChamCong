33d841fe11bd797ef70abc33ca915a967974825c
Trinh Hoang Anh <85330803+Bastian31utd@users.noreply.github.com>
2024-01-01 10:50:23 +0700

    Merge VB (#24) (#25)
    * Checkin + Home (2 view) (#19)
    
    * Update database2 (#8)
    
    * Merge pull request #1 from Bastian31utd/HT/Login
    
    First app pro
    
    * Merge pull request #1 from Bastian31utd/HT/Login
    
    First app pro
    
    * Merge pull request #1 from Bastian31utd/HT/Login
    
    First app pro
    
    ---------
    
    
    
    * Ht/login (#15)
    
    * user + admin home
    
    * Trinhh + Tam (#18)
    
    * Update check-in info
    
    * Update check-in info
    
    * Completed check-in process
    
    * Back-up
    
    * add sqllite lib
    
    * resolve duplicate
    
    * resolve conflict Trinhh + Tam
    
    ---------
    
    
    
    
    * vu bui dep trai
    
    * Done vũ
    
    * backup
    
    * Home for user + features
    
    ---------
    
    Co-authored-by: Nguyễn Quang Huy <huy43204@gmail.com>
    Co-authored-by: tamkadin <136258331+tamkadin@users.noreply.github.com>
    Co-authored-by: Vũ đẹp trai <anhvu200376@gmail.com>

--- src/main/java/com/example/cnpm/DataBaseConnector.java
+++ src/main/java/com/example/cnpm/DataBaseConnector.java
@@ -1,6 +1,15 @@
 package com.example.cnpm;
 
+import com.example.cnpm.DatabaseClass.RequestChangeSchedule;
+import com.example.cnpm.DatabaseClass.User;
+import com.example.cnpm.DatabaseClass.WorkSchedule;
+
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 
 public class DataBaseConnector {
@@ -250,6 +259,185 @@ public class DataBaseConnector {
         }
     }
 
+   public User getUserProfileFromId(String id) {
+        String sql = "SELECT *  FROM Users,Departments,Roles WHERE UserID = ? AND Users.RoleID = Roles.RoleID AND Users.DepartmentID = Departments.DepartmentID";
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String userId = resultSet.getString("UserID");
+                String name = resultSet.getString("Name");
+                String phoneNumber = resultSet.getString("PhoneNumber");
+                String email = resultSet.getString("Email");
+                String role = resultSet.getString("RoleName");
+                String department = resultSet.getString("DepartmentName");
+
+                return new User(userId, name, email, department, phoneNumber, role);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+       return null;
+   }
+    public void updateProfile(User user) {
+        String sql = "UPDATE Users SET Name = ?, PhoneNumber = ?, Email = ? WHERE UserID = ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getPhone());
+            statement.setString(3, user.getEmail());
+            statement.setString(4, user.getId());
+            statement.executeUpdate();
+            System.out.println("Update profile successfully");
+        } catch (SQLException e) {
+            System.err.println("Update profile failed: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public List<WorkSchedule> getScheduleFromIdAndTime(String id_user, int year, int month) {
+        String sql = "SELECT * FROM WorkSchedule WHERE UserID = ? AND strftime('%m', WorkDate) = ? AND strftime('%Y', WorkDate) = ?";
+        List<WorkSchedule> listWorkSchedule = new ArrayList<>();
+        System.out.println("id_user: " + id_user + " month: " + month + " year: " + year);
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, id_user);
+            statement.setString(2, String.valueOf(month));
+            statement.setString(3, String.valueOf(year));
+
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String workDateString = resultSet.getString("WorkDate"); // Get the date as a string
+                java.util.Date workDate = null;
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+                try {
+                    workDate = sdf.parse(workDateString); // Parse the string into a Date object
+                } catch (ParseException e) {
+                    e.printStackTrace(); // Handle parsing exception
+                }
+
+                String shift = resultSet.getString("Shift");
+                String id_schedule = resultSet.getString("WorkScheduleID");
+                WorkSchedule workSchedule = new WorkSchedule(id_schedule, id_user , workDate, shift);
+                listWorkSchedule.add(workSchedule);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listWorkSchedule;
+    }
+    public void sentRequestFromIdAndTime(String id_user, String type, Date date) {
+        String sql = "INSERT INTO LeaveRequest (UserID, RequestType, RequestDate, Status, RequestID) VALUES (?, ?, ?, ?, ?)";
+        try {
+            String randomID = UUID.randomUUID().toString();
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            // Chuyển đổi ngày thành chuỗi "yyyy-MM-dd"
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            String formattedDate = dateFormat.format(date);
+
+            statement.setString(1, id_user);
+            statement.setString(2, type);
+            statement.setString(3, formattedDate); // Sử dụng chuỗi đã được định dạng
+            statement.setString(4, "Pending");
+            statement.setString(5, randomID);
+            statement.executeUpdate();
+            System.out.println("Sent request successfully");
+        } catch (SQLException e) {
+            System.err.println("Sent request failed: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public List<RequestChangeSchedule> getLeaveRequestsForUser() {
+        List<RequestChangeSchedule> requests = new ArrayList<>();
+
+        String sql = "SELECT lr.RequestID, lr.UserID, lr.RequestType, lr.RequestDate, lr.Status, u.Name " +
+                "FROM LeaveRequest lr " +
+                "INNER JOIN Users u ON lr.UserID = u.UserID";
+
+        try {
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    // Lấy thông tin từ kết quả truy vấn và tạo các đối tượng RequestChangeSchedule
+                    String requestID = resultSet.getString("RequestID");
+                    String userId = resultSet.getString("UserID");
+                    String requestType = resultSet.getString("RequestType");
+                    String workDateString = resultSet.getString("RequestDate"); // Get the date as a string
+                    java.util.Date requestDate = null;
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+                    try {
+                        requestDate = sdf.parse(workDateString); // Parse the string into a Date object
+                    } catch (ParseException e) {
+                        e.printStackTrace(); // Handle parsing exception
+                    }
+                    String status = resultSet.getString("Status");
+                    String userName = resultSet.getString("Name");
+
+                    RequestChangeSchedule request = new RequestChangeSchedule(requestID, userId, requestType, requestDate, status, userName);
+                    requests.add(request);
+                }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return requests;
+    }
+    public List<RequestChangeSchedule> getLeaveRequestsForUserByUserId(String UserId) {
+        List<RequestChangeSchedule> requests = new ArrayList<>();
+
+        String sql = "SELECT lr.RequestID, lr.UserID, lr.RequestType, lr.RequestDate, lr.Status, u.Name " +
+                "FROM LeaveRequest lr " +
+                "INNER JOIN Users u ON lr.UserID = u.UserID WHERE lr.UserID = ?";
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, UserId);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                // Lấy thông tin từ kết quả truy vấn và tạo các đối tượng RequestChangeSchedule
+                String requestID = resultSet.getString("RequestID");
+                String userId = resultSet.getString("UserID");
+                String requestType = resultSet.getString("RequestType");
+                String workDateString = resultSet.getString("RequestDate"); // Get the date as a string
+                java.util.Date requestDate = null;
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+                try {
+                    requestDate = sdf.parse(workDateString); // Parse the string into a Date object
+                } catch (ParseException e) {
+                    e.printStackTrace(); // Handle parsing exception
+                }
+                String status = resultSet.getString("Status");
+                String userName = resultSet.getString("Name");
+
+                RequestChangeSchedule request = new RequestChangeSchedule(requestID, userId, requestType, requestDate, status, userName);
+                requests.add(request);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return requests;
+
+   }
+
+    public void changeStatusRequests(String RequestID, String NewStatus) {
+       String sql = "UPDATE LeaveRequest SET Status = ? WHERE RequestID = ?";
+         try {
+              PreparedStatement statement = connection.prepareStatement(sql);
+              statement.setString(1, NewStatus);
+              statement.setString(2, RequestID);
+              statement.executeUpdate();
+              System.out.println("Update status successfully");
+         } catch (SQLException e) {
+              System.err.println("Update status failed: " + e.getMessage());
+              e.printStackTrace();
+         }
+    }
 
     public static void main(String[] args) {
         DataBaseConnector connector = new DataBaseConnector();
