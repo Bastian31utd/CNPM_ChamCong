6ca754f1048368a821711ca1a687ae47e31e77b0
Trinhh <trinhhoanganh.pl@gmail.com>
2024-01-01 10:47:26 +0700

    Home for user + features

--- src/main/java/com/example/cnpm/StartView.java
+++ src/main/java/com/example/cnpm/StartView.java
@@ -3,24 +3,23 @@ package com.example.cnpm;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import javax.swing.text.html.ImageView;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StartView implements Initializable {
+    @FXML
+    Pane taskBarPane;
     private double xOffset;
     private double yOffset;
     @FXML
     private Text label;
-    @FXML Pane taskBarPane;
+
     private
     @FXML void onCheckinClick(MouseEvent event) throws IOException {
         Runtime.getRuntime()
@@ -31,11 +30,13 @@ public class StartView implements Initializable {
         com.example.cnpm.HelloApplication tmp = new com.example.cnpm.HelloApplication();
         tmp.changeScene("login.fxml");
     }
+
     @FXML
     void closeStage() {
         Stage stage = (Stage) label.getScene().getWindow();
         stage.close();
     }
+
     @FXML
     void minimizeStage() {
         Stage stage = (Stage) label.getScene().getWindow();
@@ -49,7 +50,7 @@ public class StartView implements Initializable {
             yOffset = mouseEvent.getSceneY();
         });
         taskBarPane.setOnMouseDragged(mouseEvent -> {
-            Stage stage = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+            Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
             stage.setX(mouseEvent.getScreenX() - xOffset);
             stage.setY(mouseEvent.getScreenY() - yOffset);
         });
